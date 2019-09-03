fe8713eb44ffc8c41a352742f37920fa7210f6ee
anyingiit <49945850+anyingiit@users.noreply.github.com>
2019-09-03 17:53:02 +0800

    优化了log提示格式

--- testPrg/logTest.py
+++ testPrg/logTest.py
@@ -1,5 +1,5 @@
 import logging
 if __name__ == '__main__':
     aa="nihao"
-    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] - %(asctime)s\n\tSITE:%(module)s.%(funcName)s - 5\n\tMSG :%(message)s")
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] - %(asctime)s\n>\tSITE:%(module)s.%(funcName)s - 5\n>\tMSG :%(message)s")
     logging.warning("%s",aa)
