193de863dd02f295fe124763df67ffc50c930541
anyingiit <49945850+anyingiit@users.noreply.github.com>
2019-09-03 15:47:58 +0800

    继续优化日志

--- preheattask.py
+++ preheattask.py
@@ -4,4 +4,4 @@ import logging
 def preheattask(preheatTask):
     logging.info("ready to cdn preheattask")
     preheattask = conn.cdn.create_preheat_task(**preheatTask)
-    print("preheat urls or dirs:[%s]",preheattask)
+    logging.info("preheat urls or dirs:[%s]",preheattask)
