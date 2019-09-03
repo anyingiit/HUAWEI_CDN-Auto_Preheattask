237703efc23b3646d68c7a200822cc13d8f132bd
anyingiit <49945850+anyingiit@users.noreply.github.com>
2019-09-03 16:31:44 +0800

    对log的输出格式进行了优化

--- logTest.py
+++ logTest.py
@@ -1,5 +1,5 @@
 import logging
 if __name__ == '__main__':
-    aa=111
-    logging.basicConfig(level=logging.INFO, format="[%(levelname)s]%(asctime)s:%(message)s")
-    logging.warning("%s\n\n",aa)
+    aa="nihao"
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] - %(asctime)s\n\tSITE:%(module)s.%(funcName)s - 5\n\tMSG :%(message)s")
+    logging.warning("%s",aa)
