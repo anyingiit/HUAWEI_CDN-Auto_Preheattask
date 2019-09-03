1f00f4ebfe1e6a0fe507ef6ae1a66dda95d131d2
anyingiit <49945850+anyingiit@users.noreply.github.com>
2019-09-03 19:13:52 +0800

    edit: 修改了执行预热的文案

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
             if executeCommand.websitelocaGitlCommandDef("git rev-parse master")[1] == executeCommand.websitelocaGitlCommandDef("git rev-parse origin/master")[1]:
                 nextExecuteInterval = 60
             else:
-                logging.info("|================ starting preheattask... ================|")
+                logging.info("|================ starting pull and preheattask... ================|")
                 executeCommand.websitelocaGitlCommandDef("git pull")
                 preheattaskdef()
                 lastExecuteTime = time.time()
